c6cf5ba30c0663047b512274f5a3ad1a5e2480ee
abhigarg48 <gargkumar4848@gmail.com>
2025-06-01 01:03:13 +0530

    Initial project structure from Spring Initializr

---
+++ syntexa-api/src/main/java/com/syntexa/api/SyntexaApplication.java
@@ -0,0 +1,13 @@
+package com.syntexa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntexaApplication.class, args);
+	}
+
+}
