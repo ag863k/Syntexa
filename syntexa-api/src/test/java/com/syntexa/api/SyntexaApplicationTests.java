1b6dfb064b02c7911e9fd17bca2abdeaa077cb93
abhigarg48 <gargkumar4848@gmail.com>
2025-06-06 23:46:41 +0530

    refactor: Fix test properties

--- syntexa-api/src/test/java/com/syntexa/api/SyntexaApplicationTests.java
+++ syntexa-api/src/test/java/com/syntexa/api/SyntexaApplicationTests.java
@@ -2,8 +2,10 @@ package com.syntexa.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource; 
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-h2.properties")
 class SyntexaApplicationTests {
 
     @Test
